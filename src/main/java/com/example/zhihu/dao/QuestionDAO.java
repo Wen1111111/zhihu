92dbee0eae51ff635c0924ff3ada93d204ecebdd
wencheng <1296626197@qq.com>
2020-11-15 16:57:50 +0800

    版本3：无变化

--- src/main/java/com/example/zhihu/dao/QuestionDAO.java
+++ src/main/java/com/example/zhihu/dao/QuestionDAO.java
@@ -1,4 +1,25 @@
 package com.example.zhihu.dao;
 
+
+import com.example.zhihu.model.Question;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/2.
+ */
+@Mapper
 public interface QuestionDAO {
+    String TABLE_NAME = " question ";
+    String INSERT_FIELDS = " title, content, created_date, user_id, comment_count ";
+    String SELECT_FIELDS = " id, " + INSERT_FIELDS;
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
+            ") values (#{title},#{content},#{createdDate},#{userId},#{commentCount})"})
+    int addQuestion(Question question);
+
+    List<Question> selectLatestQuestions(@Param("userId") int userId, @Param("offset") int offset,
+                                         @Param("limit") int limit);
+
 }
--- src/main/java/com/example/zhihu/dao/UserDAO.java
+++ src/main/java/com/example/zhihu/dao/UserDAO.java
@@ -2,15 +2,30 @@ package com.example.zhihu.dao;
 
 
 import com.example.zhihu.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
+/**
+ * Created by nowcoder on 2016/7/2.
+ */
 @Mapper
 public interface UserDAO {
     String TABLE_NAME = "user";
-    String INSERT_FILEDS = " name, password, salt, head_url";
-    String SELECT_FILEDS = "id, "+ INSERT_FILEDS;
-    @Insert({"insert into",TABLE_NAME,"(",INSERT_FILEDS,")" +
-            " values(#{name},#{password},#{salt},#{headUrl})"})
+    String INSET_FIELDS = " name, password, salt, head_url ";
+    String SELECT_FIELDS = " id, name, password, salt, head_url";
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSET_FIELDS,
+            ") values (#{name},#{password},#{salt},#{headUrl})"})
     int addUser(User user);
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
+    User selectById(int id);
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where name=#{name}"})
+    User selectByName(String name);
+
+    @Update({"update ", TABLE_NAME, " set password=#{password} where id=#{id}"})
+    void updatePassword(User user);
+
+    @Delete({"delete from ", TABLE_NAME, " where id=#{id}"})
+    void deleteById(int id);
 }
